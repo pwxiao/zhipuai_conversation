978ff1a7d860f2eb55683dc7ca080e5994b53e29
pwxiao <3504852205@qq.com>
2024-02-04 15:03:20 +0800

    a

---
+++ a.py
@@ -0,0 +1,26 @@
+# pip install zhipuai 请先在终端进行安装
+
+from zhipuai import ZhipuAI
+
+client = ZhipuAI(api_key="23b6ef7fd26d15dd41f2ada9368fb227.vraSfTkwHqJo7YxJ")
+print("欢迎使用智谱ai")
+a=""
+while True:
+    a = input("root：")
+    response = client.chat.completions.create(
+        model="glm-4",
+        messages=[
+            {
+                "role": "user",
+                "content": a
+            }
+        ],
+        top_p=0.7,
+        temperature=0.95,
+        max_tokens=1024,
+        stream=True,
+    )
+    print("智谱ai：",end="")
+    for trunk in response:
+        print(trunk.choices[0].delta.content,end="")
+    print()
